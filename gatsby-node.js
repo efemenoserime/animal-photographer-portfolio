48905436bc4180d6c041fe65523216ddf5e50f02
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-16 18:19:16 +0200

    Define env variables for build process w/ webpack

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ plugins, actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      plugins.define({
+        "process.env.CONTENTFUL_SPACE_ID": JSON.stringify(
+          process.env.CONTENTFUL_SPACE_ID,
+        ),
+        "process.env.CONTENTFUL_ACCESS_TOKEN": JSON.stringify(
+          process.env.CONTENTFUL_ACCESS_TOKEN,
+        ),
+      }),
+    ],
+  });
+};
