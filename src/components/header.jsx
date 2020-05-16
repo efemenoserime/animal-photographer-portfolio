0e9ecb893c24b7703bff6d1fe5ed8d4547fd2b4e
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-16 02:18:09 +0200

    Create starting point for navbar style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,45 @@
 import { Link } from "gatsby";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import React from "react";
 
 const Header = ({ siteTitle }) => (
-  <header
+  <Navbar
+    collapseOnSelect
+    variant="dark"
+    expand="lg"
     style={{
       background: "rebeccapurple",
       marginBottom: "1.45rem",
     }}
   >
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "1.45rem 1.0875rem",
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: "white",
-            textDecoration: "none",
-          }}
-        >
-          {siteTitle}
+    <Container>
+      <Navbar.Brand>
+        <Link style={{ color: "white", textDecoration: "none" }}>
+          {" "}
+          {siteTitle}{" "}
         </Link>
-      </h1>
-    </div>
-  </header>
+      </Navbar.Brand>
+      <Toggler aria-controls="responsive-navbar-nav" className="ml-auto" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Link
+            className="nav-link"
+            to="/"
+            activeStyle={{
+              color: "rgba(255, 255, 255, .80)",
+            }}
+          >
+            Home
+          </Link>
+          <Link className="nav-link" to="#link">
+            Galerie
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 Header.propTypes = {
@@ -38,5 +49,16 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: "",
 };
+
+const Toggler = styled(Navbar.Toggle)`
+  border: none;
+  color: "white";
+
+  &:focus,
+  &:visited {
+    border: none;
+    outline: none;
+  }
+`;
 
 export default Header;
