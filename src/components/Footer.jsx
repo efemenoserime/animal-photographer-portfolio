c9dc3da50343a2e87a5ca77595bd06655f6b83e6
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-17 14:22:05 +0200

    Style footer component using styled-component
    * Comment footer component
    * Create footer link which redirects to personal page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,20 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Container } from "react-bootstrap";
 
+/**
+ * Simple footer component
+ * @param {String} className Set footer classes trough className prop
+ * @param {Object} style Inline styles applied to footer component
+ * @author Efeme Noserime
+ */
 const Footer = ({ className, style }) => {
   return (
     <Foot style={{ background: "#814fb4", ...style }} className={className}>
-      <Container className="pt-5 pb-5 text-light">
-        © {new Date().getFullYear()}, Built with{" "}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <Container className="pt-5 pb-5 text-light d-flex justify-content-center">
+        © {new Date().getFullYear()}, Built by{"  "}
+        <FooterLink target="_blank" href="https://www.efeme-noserime.me">
+          Efeme Noserime
+        </FooterLink>
       </Container>
     </Foot>
   );
@@ -22,6 +30,22 @@ Footer.propTypes = {
 const Foot = styled.footer`
   background-color: "#814fb4";
   color: "#ffff33";
+`;
+
+const FooterLink = styled.a`
+  color: #f3f3f3 !important;
+  font-weight: 600;
+  text-decoration: none;
+
+  margin-left: 0.45rem;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    color: #fff;
+  }
 `;
 
 export default Footer;
