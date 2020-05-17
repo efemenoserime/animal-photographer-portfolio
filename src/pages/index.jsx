7982d4350e6fa322f22c600f2b32c70906dbc75f
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-17 22:08:59 +0200

    Include GalleryPreview in index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import HeroSlider from "../components/HeroSlider";
+import GalleryPreview from "../components/GalleryPreview";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSlider />
+    <GalleryPreview />
   </Layout>
 );
 
