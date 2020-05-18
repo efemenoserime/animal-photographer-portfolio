0f5a0a508a43208552ce0030a4830a1fc4ab6cb7
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-18 04:14:30 +0200

    Configure gatsby-node to create pages for gallery

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path");
+
 exports.onCreateWebpackConfig = ({ plugins, actions }) => {
   actions.setWebpackConfig({
     plugins: [
@@ -17,5 +20,43 @@ exports.onCreateWebpackConfig = ({ plugins, actions }) => {
         ),
       }),
     ],
+  });
+};
+
+// Create paginated pages for gallery
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allContentfulImage {
+        nodes {
+          id
+          title
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild("Error while running GraphQL query.");
+    return;
+  }
+
+  const images = result.data.allContentfulImage.nodes;
+  const imagesPerPage = 9;
+
+  // Calculate how much pages are needed to show all images
+  const numPages = Math.ceil(images.length / imagesPerPage);
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? "/gallery" : `/gallery/${i + 1}`,
+      component: path.resolve("./src/templates/gallery-template.jsx"),
+      context: {
+        limit: imagesPerPage,
+        skip: i * imagesPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
   });
 };
