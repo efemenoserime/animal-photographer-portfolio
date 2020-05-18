ba76de7f36fe14bd7c46f6efb009cb21202b4e47
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-18 12:17:54 +0200

    Add styled-component plugin to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
       },
     },
     "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-manifest",
