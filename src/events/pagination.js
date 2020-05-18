5b7e2c1deb8be40935b0ce6d1d7f0e65911ad310
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-18 11:11:32 +0200

    Create handlers for all pagination buttons

--- src/events/pagination.js
+++ src/events/pagination.js
@@ -1,10 +1,54 @@
 import { navigate } from "@reach/router";
 
 /**
- * onClick event handler for pagination items
+ * onClick event handler for pagination index items
  * @param {String} path Base path to navigate to
- * @param {Number} page Integer indexing to paginated page
+ * @param {Number} page Integer value indexing to paginated page
  */
-export const onClick = (path, page) => {
+export const onClickIndex = (path, page) => {
   navigate(`${path}/${page !== 1 ? page : ""}`);
+};
+
+/**
+ * Handles navigation to next page
+ * @param {String} path  Base path to navigate to
+ * @param {Number} nextPage Integer referencing next page
+ */
+export const onClickNext = (path, nextPage) => {
+  navigate(`${path}/${nextPage}`);
+};
+
+/**
+ * Handles navigation to previous page
+ * @param {String} path Base path to navigate to
+ * @param {Number} previousPage Integer referencing next page
+ */
+export const onClickPrevious = (path, previousPage) => {
+  navigate(`${path}/${previousPage === 1 ? "" : previousPage}`);
+};
+
+/**
+ * Navigates to last pagination page
+ * @param {String} path Base path to navigate to
+ * @param {Number} lastPage Integer referencing next page
+ */
+export const onClickLast = (path, lastPage) => {
+  navigate(`${path}/${lastPage}`);
+};
+
+/**
+ * Navigates to last pagination page
+ * @param {String} path Base path to navigate to
+ * @param {Number} lastPage Integer referencing next page
+ */
+export const onClickFirst = path => {
+  navigate(`${path}`);
+};
+
+export default {
+  onClickIndex,
+  onClickNext,
+  onClickLast,
+  onClickPrevious,
+  onClickFirst,
 };
