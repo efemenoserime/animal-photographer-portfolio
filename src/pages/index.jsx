3f832c0e9c68ee6c8636187962b680a2d3ae622f
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-19 02:47:39 +0200

    Add AboutSection to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import HeroSlider from "../components/HeroSlider";
 import GalleryPreview from "../components/GalleryPreview";
+import AboutSection from "../components/AboutSection";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSlider />
     <GalleryPreview />
+    <AboutSection />
   </Layout>
 );
 
