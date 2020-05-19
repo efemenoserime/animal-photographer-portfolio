87c880e99b0c46d8c33934af401cffa32b9f953e
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-19 09:54:26 +0200

    Replace GalleryPreview with GalleryCategories

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import HeroSlider from "../components/HeroSlider";
-import GalleryPreview from "../components/GalleryPreview";
+import GalleryCategories from "../components/GalleryCategories";
 import AboutSection from "../components/AboutSection";
+
+//import ContactForm from "../components/ContactForm";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSlider />
-    <GalleryPreview />
+    <GalleryCategories />
     <AboutSection />
+
+    {/*<ContactForm /> */}
   </Layout>
 );
 
