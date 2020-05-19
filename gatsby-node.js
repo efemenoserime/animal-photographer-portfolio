420eaa46af5dd134c31738786a3d21ca80b95c41
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-19 09:55:57 +0200

    Create seprate routes for each category in gallery

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,7 +24,9 @@ exports.onCreateWebpackConfig = ({ plugins, actions }) => {
   });
 };
 
-// Create paginated pages for gallery
+/**
+ * Creates all gallery pages
+ */
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
   const result = await graphql(`
@@ -55,6 +57,117 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         limit: imagesPerPage,
         skip: i * imagesPerPage,
         numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
+
+  /** Query for horse images and create pages**/
+  const horseResult = await graphql(`
+    {
+      allContentfulImage(filter: { category: { eq: "Horses" } }) {
+        nodes {
+          title
+          id
+          image {
+            fluid {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const horseImages = horseResult.data.allContentfulImage.nodes;
+
+  // Calculate how much pages are needed to show all images
+  const numPagesHorses = Math.ceil(horseImages.length / imagesPerPage);
+  Array.from({ length: numPagesHorses }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? "/gallery/horses" : `/gallery/horses/${i + 1}`,
+      component: path.resolve("./src/templates/gallery-horse-template.jsx"),
+      context: {
+        limit: imagesPerPage,
+        skip: i * imagesPerPage,
+        numPagesHorses,
+        currentPage: i + 1,
+      },
+    });
+  });
+
+  /** Query for cat images and create pages**/
+  const catResult = await graphql(`
+    {
+      allContentfulImage(filter: { category: { eq: "Cats" } }) {
+        nodes {
+          title
+          id
+          image {
+            fluid {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const catImages = catResult.data.allContentfulImage.nodes;
+
+  // Calculate how much pages are needed to show all images
+  const numPagesCats = Math.ceil(catImages.length / imagesPerPage);
+  Array.from({ length: numPagesCats }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? "/gallery/cats" : `/gallery/cats/${i + 1}`,
+      component: path.resolve("./src/templates/gallery-cat-template.jsx"),
+      context: {
+        limit: imagesPerPage,
+        skip: i * imagesPerPage,
+        numPagesCats,
+        currentPage: i + 1,
+      },
+    });
+  });
+
+  /** Query for dog images and create pages**/
+  const dogResult = await graphql(`
+    {
+      allContentfulImage(filter: { category: { eq: "Dogs" } }) {
+        nodes {
+          title
+          id
+          image {
+            fluid {
+              base64
+              tracedSVG
+              srcWebp
+              srcSetWebp
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const dogImages = dogResult.data.allContentfulImage.nodes;
+
+  // Calculate how much pages are needed to show all images
+  const numPagesDogs = Math.ceil(dogImages.length / imagesPerPage);
+  Array.from({ length: numPagesDogs }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? "/gallery/dogs" : `/gallery/dogs/${i + 1}`,
+      component: path.resolve("./src/templates/gallery-dog-template.jsx"),
+      context: {
+        limit: imagesPerPage,
+        skip: i * imagesPerPage,
+        numPagesDogs,
         currentPage: i + 1,
       },
     });
