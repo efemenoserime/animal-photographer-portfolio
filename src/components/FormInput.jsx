7c1fc71f3c6aee5ff56a096c2b42d15bf294771d
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-19 07:12:18 +0200

    Add `as` prop to FormInput

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -5,17 +5,22 @@ import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
 /**
  * Render FormInput component (Bootstrap FormControl <https://react-bootstrap.github.io/components/forms/#form-control-props>)
  * @param {("text"|"email"|"password")} type Specifies input type that should be rendered. If none provided, fallback to text input.
- * @param {String} label Defines label text for appropriate FormControl. If none provided, no label will be rendered.
+ * @param {string} label Defines label text for appropriate FormControl. If none provided, no label will be rendered.
  * @param {string} name Name for form input component
+ * @param {string} as Determine what kind of form component shall be rendered
  * @author Efeme Noserime <noserimeefeme@gmail.com>
  */
-function FormInput({ type, label, name }) {
+function FormInput({ type, label, name, as }) {
   return (
     <FormGroup>
       {/* Only render FormLabel if label was specified */}
       {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
       {/* Fallback to 'text' as input type if none specified */}
-      <FormControl name={name} type={type ? type : "text"}></FormControl>
+      <FormControl
+        as={as}
+        name={name}
+        type={type ? type : "text"}
+      ></FormControl>
     </FormGroup>
   );
 }
@@ -24,6 +29,7 @@ FormInput.propTypes = {
   type: PropTypes.string,
   label: PropTypes.string,
   name: PropTypes.string,
+  as: PropTypes.string,
 };
 
 export default FormInput;
