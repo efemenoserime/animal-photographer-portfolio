28e42780cde2d69f11aaaf1ff2f0fcc7eccdd1d3
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-20 14:41:35 +0200

    Create custom pagination items based on AniLink
    * items navigate with transition

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,58 +1,63 @@
 import React from "react";
 import { Pagination } from "react-bootstrap";
 import PropTypes from "prop-types";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import styled from "styled-components";
 
-const ImagePagination = ({
-  path,
-  pageContext,
-  numPages,
-  paginationHandlers,
-}) => {
-  // Initialize array for paginationItems
-  const paginationItems = [];
+const ImagePagination = ({ path, pageContext, numPages }) => {
+  const { currentPage } = pageContext;
 
-  // Push pagination index into 'paginationItems'
+  const activeStyle = { background: "#3D314A", color: "#fff" };
+
+  // Initialize array for paginationKeys
+  const paginationKeys = [];
+
+  // Push pagination index into 'paginationKeys'
   for (let i = 0; i < numPages; i++) {
-    paginationItems.push(i + 1);
+    paginationKeys.push(i + 1);
   }
-
+  // TODO Create pagination components to navigate back and forth between pages
   return (
-    <Pagination>
-      <Pagination.First onClick={() => paginationHandlers.onClickFirst(path)} />
-      <Pagination.Prev
-        disabled={pageContext.currentPage === 1 ? true : false}
-        onClick={() =>
-          paginationHandlers.onClickPrevious(path, pageContext.currentPage - 1)
-        }
-      />
-      {paginationItems.map(item => (
-        <Pagination.Item
-          href=""
-          key={item}
-          onClick={() => paginationHandlers.onClickIndex(path, item)}
-          active={pageContext.currentPage === item ? true : false} // active if currentPage matches pagination index
-        >
-          {item}
-        </Pagination.Item>
+    <Paginate className="my-3">
+      {paginationKeys.map(pageIndex => (
+        <li key={pageIndex}>
+          {" "}
+          <PaginationItem
+            style={currentPage === pageIndex ? activeStyle : null}
+            cover
+            to={`${path}/${pageIndex}`}
+          >
+            {pageIndex}
+          </PaginationItem>{" "}
+        </li>
       ))}
-      <Pagination.Next
-        disabled={pageContext.currentPage === numPages ? true : false}
-        onClick={() =>
-          paginationHandlers.onClickNext(path, pageContext.currentPage + 1)
-        }
-      />
-      <Pagination.Last
-        onClick={() => paginationHandlers.onClickLast(path, numPages)}
-      />
-    </Pagination>
+    </Paginate>
   );
 };
 
 ImagePagination.propTypes = {
   path: PropTypes.string.isRequired,
   pageContext: PropTypes.object.isRequired,
-  paginationHandlers: PropTypes.objectOf(PropTypes.func),
   numPages: PropTypes.number.isRequired,
 };
+
+const Paginate = styled(Pagination)`
+  margin: 1rem 0;
+`;
+
+const PaginationItem = styled(AniLink)`
+  padding: 0.75rem;
+  border: thin solid #d3d3d3;
+
+  &:hover {
+    background: #a393bf;
+    color: #fff;
+  }
+
+  &:active {
+    background: #3d314a;
+    color: #fff;
+  }
+`;
 
 export default ImagePagination;
