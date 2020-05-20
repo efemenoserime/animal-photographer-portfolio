3ebce16a2c7f4e9cd2355dc41a86b36aadc54907
Efeme Noserime <noserimeefeme@gmail.com>
2020-05-20 17:23:25 +0200

    Create reusable GalleryImage component

---
+++ src/components/GalleryImage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Col } from "react-bootstrap";
+import styled from "styled-components";
+import Img from "gatsby-image";
+
+/**
+ *
+ * Gallery image held by a column component.
+ * @param {string} fluid Image data provided through a gatsby query
+ * @param {string} className Classes applied to image container
+ * @param {string} style Inline styles for image container
+ * @param {string} onClick Click event handler
+ *
+ * NOTE: All passed properties that weren't defined in the docs will be set
+ * directly to the underlying Image component.
+ */
+const GalleryImage = ({ fluid, className, style, onClick }) => {
+  return (
+    <ImageContainer
+      className={className}
+      style={style}
+      onClick={() => onClick()}
+    >
+      <Img
+        fluid={fluid}
+        style={{ height: "80vh", width: "100%" }}
+        className="h-100"
+        imgStyle={{ objectFit: "contain" }}
+      />
+    </ImageContainer>
+  );
+};
+
+GalleryImage.propTypes = {
+  fluid: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
+
+const ImageContainer = styled(Col)`
+  margin-bottom: 16px;
+  width: 100%;
+  min-height: 260px;
+`;
+
+export default GalleryImage;
